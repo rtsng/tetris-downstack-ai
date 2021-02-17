4f02eb87c2eba88c71ed24439a9f6c1e9dd85461
rtsng <ryan.j.tsang@gmail.com>
2021-02-17 03:22:50 -0500

    frameworks?

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 print("hello world")
+
+# make a working ai
+# ------------------------------------
+
+# working game
+#     main game loop
+#     visuals
+#     core mechanics
+#     collision detection
+#     input detection
+#     implement hold
+#     implement randomized 7 bag
+#     implement srs
+
+
+# actual ai
+#     primary goal is to get to the bottom in the minimum number of pieces
+
+
+
+
+
